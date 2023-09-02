1bfddd3be5594e8a6705c8563821ab16d4ba77b6
cidit <cidit>
2023-09-02 19:20:26 -0400

    found a way to animate continuously, updt journal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import numpy as np
 import sqlite3
 
@@ -14,4 +15,30 @@ def simple_plot():
 
 if __name__ == "__main__":
     print("Hello, World!")
-    simple_plot()
+
+    matrix = np.array([[0.8, 2.4, 2.5, 3.9, 0.0, 4.0, 0.0],
+                    [2.4, 0.0, 4.0, 1.0, 2.7, 0.0, 0.0],
+                    [1.1, 2.4, 0.8, 4.3, 1.9, 4.4, 0.0],
+                    [0.6, 0.0, 0.3, 0.0, 3.1, 0.0, 0.0],
+                    [0.7, 1.7, 0.6, 2.6, 2.2, 6.2, 0.0],
+                    [1.3, 1.2, 0.0, 0.0, 0.0, 3.2, 5.1],
+                    [0.1, 2.0, 0.0, 1.4, 0.0, 1.9, 6.3]])
+    
+    fig, ax = plt.subplots()
+    xdata, ydata = [], []
+    ln, = ax.plot([], [], 'r+')
+
+    def init():
+        ax.set_xlim(0, 2*np.pi)
+        ax.set_ylim(-1, 1)
+        return ln,
+
+    def update(frame):
+        xdata.append(frame)
+        ydata.append(np.sin(frame))
+        ln.set_data(xdata, ydata)
+        return ln,
+
+    ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128),
+                        init_func=init, blit=True)
+    plt.show()
