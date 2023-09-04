04eadb173b2b834bef4d4b6acd4ea123a898a303
cidit <cidit>
2023-09-04 10:25:09 -0400

    success

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sqlite3
 if __name__ == "__main__":
     print("Hello, World!")
 
-    matrix = np.rint(np.random.rand(100, 100)).astype(int)
+    matrix = np.random.rand(100, 100)
     zeros = np.zeros((100, 100), dtype=int)
     print(zeros)
     print(matrix)
@@ -15,19 +15,18 @@ if __name__ == "__main__":
     fig, ax = plt.subplots()
     xdata, ydata = [], []
     # static_hm = ax.imshow(matrix, cmap="Wistia", alpha=0.25)
-    dynamic_hm = ax.imshow(zeros, zorder=1, alpha=0.25)
+    dynamic_hm = ax.imshow(zeros, zorder=1, alpha=0.25, vmin=0, vmax=1)
     
-    def init():
-        return (dynamic_hm)
     
     def update(frame):
-        new_matrix = np.rint(np.random.rand(100, 100)).astype(int) 
+        new_matrix = np.random.rand(100, 100) 
         dynamic_hm.set_data(new_matrix)
         return (dynamic_hm),
 
     # reference to `ani` must be kept alive because it needs to maintain an internal timer that's gonna get garbage collected otherwise.
     ani = FuncAnimation(fig, update,  
                         frames=None,
-                        blit=True, interval=1000)
+                        blit=True, 
+                        cache_frame_data=False)
     
     plt.show()
