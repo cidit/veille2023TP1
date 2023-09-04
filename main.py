18444e11eee5479cd4d8eb23804fda846d2b5143
cidit <cidit>
2023-09-03 23:29:37 -0400

    blocking

--- main.py
+++ main.py
@@ -16,23 +16,21 @@ def simple_plot():
 if __name__ == "__main__":
     print("Hello, World!")
 
-    matrix = np.random.rand(100, 100)
+    matrix = np.zeros((100, 100))
     
     fig, ax = plt.subplots()
     xdata, ydata = [], []
-    static_hm = ax.imshow(matrix, cmap="Wistia", alpha=0.25)
+    # static_hm = ax.imshow(matrix, cmap="Wistia", alpha=0.25)
     dynamic_hm = ax.imshow(matrix, zorder=1, cmap="magma_r", alpha=0.25)
     
-    def init():
-        # ax.set_xlim(0, 2*np.pi)
-        # ax.set_ylim(-1, 1)
-        return dynamic_hm,
-
     def update(frame):
         new_matrix = np.random.rand(100, 100)
+        print(new_matrix[0:10])
         dynamic_hm.set_data(new_matrix)
         return dynamic_hm,
 
+    # reference to `ani` must be kept alive because it needs to maintain an internal timer that's gonna get garbage collected otherwise.
     ani = FuncAnimation(fig, update, frames=None,
-                        init_func=init, blit=True)
+                        blit=True)
+    
     plt.show()
